83f773f843ebf3ff9722b43fd1fa70f4a2a55d85
JanJereczek <jan.jereczek@gmail.com>
2024-09-24 16:08:44 +0200

    export more functions to be tested in velocities3D

--- src/Pagos.jl
+++ src/Pagos.jl
@@ -11,15 +11,22 @@ include("structs/params.jl")
 include("structs/options.jl")
 # include("structs/tools.jl")
 include("structs/icesheet.jl")
+export Domain, State, Params, Options, IceSheet
 
 include("helpers/indices.jl")
 include("helpers/staggering.jl")
 include("helpers/debug.jl")
+include("helpers/sigmatransform.jl")
+export exponential_vertical_layers, sigma
 
-include("dynamics/velocities3D.jl")
 include("dynamics/friction/plastic.jl")
 include("dynamics/friction/stagger.jl")
 include("dynamics/advection.jl")
+
+include("dynamics/velocities3D.jl")
+export aggregate_viscosity_integral!, aggregated_viscosity_integral!
+export velocities3D!, surface_velocity!, depthaveraged_velocity!
+export basal_velocity_from_surface_velocity!, depthavg_velocity!
 
 include("material/stress.jl")
 include("material/strainrate.jl")
@@ -27,14 +34,10 @@ include("material/strainrate.jl")
 include("numerics/differences.jl")
 include("numerics/picard.jl")
 include("numerics/pseudotransient.jl")
-
-include("thermodynamics/viscosity.jl")
-
-export Domain, State, Params, Options, IceSheet
 export stagger_beta!
 export pseudo_dotvel!, pseudo_vel!, pseudo_transient!
+
+include("thermodynamics/viscosity.jl")
 export delx, dely, advect!
-export aggregate_viscosity_integral!, velocities3D!
-export surface_velocity!, depthaveraged_velocity!
 
 end
