28ffa129fdbb2a6d44417ffa4f6d168e0832c1af
JanJereczek <jan.jereczek@gmail.com>
2024-10-03 15:35:00 +0200

    expand params struct --> split in future

--- src/structs/params.jl
+++ src/structs/params.jl
@@ -2,7 +2,11 @@
     Params{T<:AbstractFloat}
 
 Struct containing the parameters of the ice sheet model, which contains:
+- `ndim1::Int`: the dimension coefficient for 1D problems.
+- `ndim2::Int`: the dimension coefficient for 2D problems.
+- `ndim3::Int`: the dimension coefficient for 3D problems.
 - `rho_ice::T`: the density of ice.
+- `min_bulk_viscosity_ice::T`: the minimal bulk viscosity of ice.
 - `muB::T`: the bulk viscosity of ice.
 - `g::T`: the gravitational acceleration.
 
@@ -13,11 +17,15 @@ params = Params{Float64}()
 
 Example using user-defined values:
 ```julia
-params = Params{Float64}(910.0, 0.5, 9.81)
+params = Params{Float64}(rho_ice = 910.0)
 ```
 """
 @kwdef struct Params{T<:AbstractFloat}
-    rho_ice::T = T(910.0)       # ice density
-    muB::T = T(1e2)             # bulk viscosity of ice
-    g::T = T(9.81)              # gravitational acceleration
+    ndim1::Int = 2.1
+    ndim2::Int = 4.1
+    ndim3::Int = 6.1
+    rho_ice::T = T(910.0)               # ice density
+    min_bulk_viscosity_ice::T = T(0.5)  # minimal bulk viscosity of ice
+    muB::T = T(1e2)                     # (old) bulk viscosity of ice
+    g::T = T(9.81)                      # gravitational acceleration
 end
