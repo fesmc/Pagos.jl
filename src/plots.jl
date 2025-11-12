bfd1e6b334cad03cb70e5825c3747b5ecf4587e2
JanJereczek <jan.jereczek@gmail.com>
2025-11-12 13:27:06 +0100

    init makieext

---
+++ src/plots.jl
@@ -0,0 +1,20 @@
+# PagosMakieExt
+
+"""
+$(TYPEDSIGNATURES)
+"""
+function plot_rate_factor end
+export plot_rate_factor
+
+"""
+$(TYPEDSIGNATURES)
+"""
+function plot_melting_point end
+export plot_melting_point
+
+"""
+$(TYPEDSIGNATURES)
+"""
+function plot_ice_viscosity end
+export plot_ice_viscosity
+
