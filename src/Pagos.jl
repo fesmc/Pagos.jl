dff9782f095ac2f0b04f3a9d1606497a141394e7
JanJereczek <jan.jereczek@gmail.com>
2026-05-27 18:16:21 +0200

    Refactor exports and include statements for clarity and organization

--- src/Pagos.jl
+++ src/Pagos.jl
@@ -7,7 +7,6 @@ using LinearAlgebra
 using LinearSolve           # TODO: externalize
 using StatsBase
 using SparseArrays
-# using CUDA
 
 ###########################################################
 # Structs
@@ -28,8 +27,10 @@ export Domain, State, Params, Options, IceSheet
 include("utils/indices.jl")
 include("utils/math.jl")
 include("utils/debug.jl")
+include("utils/mask.jl")
+export ActiveCellsMap, active_indices!, apply!
 
-# include("helpers/indices.jl")
+include("helpers/indices.jl")
 # include("helpers/staggering.jl")
 
 ###########################################################
@@ -37,13 +38,17 @@ include("utils/debug.jl")
 ###########################################################
 
 include("topography/sigmatransform.jl")
-export AbstractSigmaTransform, PowerSigmaTransform, ArctanSigmaTransform
+export AbstractSigmaTransform, PowerSigmaTransform
+export ArctanSigmaTransform, LinearSigmaTransform
+export QuadraticSigmaTransform
 export VerticalLayering, CorrectedVerticalLayering
 export ζ_aa, ζ_ac, sigma
 
 include("topography/calving.jl")
-export AbstractCalving, RelaxedCalving, ThicknessCalving, LipscombCalving
-export LevermannCalving, CrawfordCalving, BassisCalving, BedStddevCalving
+export AbstractCalving, AbstractGroundedCalving, AbstractFloatingCalving
+export ConstantCalving, RelaxedCalving, ThicknessCalving
+export LipscombCalving, LevermannCalving, CrawfordCalving, BassisCalving
+export EigenCalving, FlotationCalving, PollardDeContoCalving
 export calving_rate, calving_rate!
 
 ###########################################################
@@ -57,15 +62,20 @@ export LeguyEffectivePressure, TillEffectivePressure
 export effective_pressure, effective_pressure!
 
 include("dynamics/basal_friction.jl")
-export AbstractBasalFriction, ConstantBetaBasalFriction
-export LinearBetaBasalFriction
-export PseudoPlasticPowerBasalFriction, RegularizedCoulombBasalFriction
+export AbstractBasalBeta, ConstantBasalBeta
+export PseudoPlasticPowerBasalBeta, CoulombBasalBeta
 export basal_shear_stress, basal_shear_stress!
 
 include("dynamics/velocities3D.jl")
 export aggregate_viscosity_integral!, aggregated_viscosity_integral!
 export velocities3D!, surface_velocity!, depthaveraged_velocity!
 export basal_velocity_from_surface_velocity!, depthavg_velocity!
+
+include("dynamics/pseudotransient.jl")
+export pseudo_transient!
+
+include("dynamics/inertial.jl")
+export inertial_velocity!
 
 include("dynamics/friction/plastic.jl")
 include("dynamics/friction/stagger.jl")
@@ -77,13 +87,13 @@ include("dynamics/advection.jl")
 
 include("material/rate_factor.jl")
 export AbstractRateFactor, ConstantRateFactor, ArrheniusRateFactor
-export SmithMorlandRateFactor
+export SmithMorlandRateFactor, HookeRateFactor, LliboutryDuvalRateFactor
 export rate_factor, rate_factor!
 
-include("material/creep_function.jl")
-export AbstractCreepFunction, GlenNyeCreepFunction, RegularizedGlenNyeCreepFunction
-export SmithMorlandCreepFunction
-export creep_function, creep_function!
+include("material/creep.jl")
+export AbstractCreep, GlenNyeCreep, RegularizedGlenNyeCreep
+export SmithMorlandCreep, PettitWaddingtonCreep, GoldsbyKohlstedtCreep
+export creep, creep!
 
 include("material/flow_law.jl")
 export AbstractFlowLaw, ConstantViscosityFlowLaw, RateCreepFlowLaw
@@ -92,8 +102,10 @@ export viscosity, viscosity!
 
 include("material/pressure_melting_point.jl")
 export AbstractPressureMeltingPoint, LinearPressureMeltingPoint
-export melting_point, melting_point!
+export ConstantPressureMeltingPoint, LinearSalinityPressureMeltingPoint
+export pressure_melting_point, pressure_melting_point!
 export relative_temperature, relative_temperature!
+export thermal_forcing, thermal_forcing!
 
 include("material/stress.jl")
 include("material/strainrate.jl")
@@ -107,7 +119,7 @@ include("numerics/picard.jl")
 # include("numerics/pseudotransient.jl")
 # export stagger_beta!
 # export pseudo_dotvel!, pseudo_vel!, pseudo_transient!
-# export delx, dely, advect!
+export delx1, delx2, delx1!, delx2!
 
 ###########################################################
 # Extensions
