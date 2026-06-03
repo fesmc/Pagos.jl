1719f05d6f2c684fdd5665aeace5ecba3e48d73d
JanJereczek <jan.jereczek@gmail.com>
2026-06-03 12:12:59 +0200

    correct = -> ==

--- test/utils/indices.jl
+++ test/utils/indices.jl
@@ -1,10 +1,15 @@
-i1, i2 = 1, 10
-strict = StrictIndexing(i1, i2)
-flat = FlatIndexing(i1, i2)
-reflective = ReflectiveIndexing(i1, i2)
-periodic = PeriodicIndexing(i1, i2)
+using Pagos
+using Test
 
-@test index(2, -1, strict) = 1
-@test index(1, -3, flat) = 1
-@test index(10, 1, reflective) = 9
-@test index(1, -1, periodic) = 10
+@testset "AbstractIndexing" begin
+    i1, i2 = 1, 10
+    strict     = StrictIndexing(i1, i2)
+    flat       = FlatIndexing(i1, i2)
+    reflective = ReflectiveIndexing(i1, i2)
+    periodic   = PeriodicIndexing(i1, i2)
+
+    @test index(2, -1, strict)     == 1
+    @test index(1, -3, flat)       == 1
+    @test index(10, 1, reflective) == 9
+    @test index(1, -1, periodic)   == 10
+end
